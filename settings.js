ca625aa0a6b160105a9c7057b23a797bf4bdf9fc
LSsithu <140079494+LSsithu@users.noreply.github.com>
2024-10-05 08:15:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==QT5QXOlNHeItUbvhlZ3dFcUdHdBZlT0oldrdHM51mM3kEcSZUNENXbxIjdjIGcZhnRslEbpwd=68931906749',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
@@ -19,14 +19,14 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
+  MONGODB_URL: 'mongodb://mongo:RjWPJDvhGTgekQmGipyDnJvGQccFYQGU@autorack.proxy.rlwy.net:44659', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
   BOT_NAME: '𝐐𝐔𝐄𝐄𝐍 𝐄𝐋𝐈𝐒𝐀',
-  OWNER_NAME: 'ᴍ ʀ  ɴ ɪ ᴍ ᴀ',
-  OWNER_NUMBER: '94715166712',
-  SUDO: '94715166712',
+  OWNER_NAME: 'ᴍᴀꜱᴛᴇʀ ʀᴏᴄᴋʏ',
+  OWNER_NUMBER: '94704104383',
+  SUDO: '94704104383',
   WORK_TYPE: 'public',
   WORK_MODE: 'group',
   TIME_ZONE: 'Asia/Colombo',
